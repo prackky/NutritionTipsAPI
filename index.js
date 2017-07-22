977c24c57a118617ee0fe0b8f09d0b7b3e4fdf01
Prakhar Rastogi <rastogi.prakhar@live.com>
2017-07-22 10:01:11 +0530

    ok

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var nutritionTips = require('./tips.json');
+var motivation = require('./motivateURL.json');
 const app = express();
 
 const REST_PORT = (process.env.PORT || 3000);
@@ -16,5 +17,32 @@ app.get('/nutrition', function(req, res) {
     console.log(ran);
     jsonResponse.push({ "text": nutritionTips[ran].tip});
     console.log(nutritionTips[ran].tip);
+    res.send(jsonResponse);
+});
+
+app.get('/motivation', function(req, res) {
+    var jsonResponse = [];
+    //console.log(tip);
+    var ran = Math.floor((Math.random() * 10));
+    console.log(ran);
+    jsonResponse.push({
+      "attachment": {
+        "type": "image",
+        "payload": {
+          "url": motivation[ran].url
+        }
+      },
+      "quick_replies": [
+          {
+          "title":"Main Menu",
+          "block_names":["Default answer"]
+        },
+        {
+          "title":"More",
+          "block_names":["Motivate me"]
+        }
+      ]}
+      );
+    console.log(motivation[ran].url);
     res.send(jsonResponse);
 });
