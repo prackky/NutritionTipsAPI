49cd4e7e2286f6d1644abe4aa9a8767d75f8c88b
Prakhar Rastogi <rastogi.prakhar@live.com>
2017-08-25 23:04:58 +0530

    mongo

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express();
 
 const REST_PORT = (process.env.PORT || 3000);
 var uristring = 
-  process.env.MONGODB_URI || 
-  'mongodb://pracky:genuinely@knowshipp-shard-00-00-ik0ru.mongodb.net:27017,knowshipp-shard-00-01-ik0ru.mongodb.net:27017,knowshipp-shard-00-02-ik0ru.mongodb.net:27017/gymboDB?ssl=true&replicaSet=knowshipp-shard-0&authSource=admin';
+  process.env.MONGODB_URI;
 
 app.use(bodyParser.json());
 
