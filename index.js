4b1632c29f268b24f5a05a48d057a509290a600c
Prakhar Rastogi <rastogi.prakhar@live.com>
2017-08-25 22:55:02 +0530

    node_modules deleted

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const REST_PORT = (process.env.PORT || 3000);
 var uristring = 
   process.env.MONGODB_URI || 
   'mongodb://pracky:genuinely@knowshipp-shard-00-00-ik0ru.mongodb.net:27017,knowshipp-shard-00-01-ik0ru.mongodb.net:27017,knowshipp-shard-00-02-ik0ru.mongodb.net:27017/gymboDB?ssl=true&replicaSet=knowshipp-shard-0&authSource=admin';
-//app.use(bodyParser.json());
+
+app.use(bodyParser.json());
 
 mongoose.connect(uristring, function (err, res) {
   if (err) { 
@@ -17,10 +18,6 @@ mongoose.connect(uristring, function (err, res) {
   } else {
     console.log ('Succeeded connected to: ' + uristring);
   }
-});
-
-app.listen(REST_PORT, function() {
-    console.log('Bot-Server listening on port ' + REST_PORT);
 });
 
 app.get("/playMeditation", function (request, response){
@@ -36,4 +33,8 @@ app.get('/yogaTips', callback.yogaTipsCB);
 app.get('/user', user.userSave);
 
 app.get('/videoChannel', youtube.youtubeController);
+
+app.listen(REST_PORT, function() {
+    console.log('Bot-Server listening on port ' + REST_PORT);
+});
 
--- libs/youtube.js
+++ libs/youtube.js
@@ -2,13 +2,14 @@ var request = require('request');
 var API_END_POINT = 'https://www.googleapis.com/youtube/v3/search';
 
 var callAPI = function(resource, qs, callback) {
-
+    // create URL with user provided parameters
     if(resource == 'videoSearch'){
-        //console.log("value of qs "+qs.res_id);
-        resource = "?" + "part=snippet&order=viewcount&" + "q="+ qs.search+ "&type=video&videoDefinition=high&";
+        resource = "?" + "part="+ qs.part +"&order=" + qs.order + "&q="+ qs.search + 
+                   "&type="+ qs.type +"&videoDefinition=" + qs.videoDefinition + "&videoType=" + qs.videoType + 
+                    "&maxResults=" + qs.maxResults +"&";
         console.log(resource);
     }
-
+    
     var options = {
         method: 'GET',
         url: API_END_POINT + resource + "key=" + YOUTUBE_ACCESS_TOKEN,
@@ -38,54 +39,6 @@ var Youtube = function(user_key) {
     // Common APIs
     this.getVideoSearch = function(options, callback) {
         callAPI('videoSearch', options, callback);
-    };
-
-    // Location Specific APIs
-    this.getGeoCode = function(latitide, longitude, callback) {
-        callAPI('/geocode', {
-            lat: latitide,
-            lon: longitude
-        }, callback);
-    };
-
-    this.getLocationDetails = function(entityId, entityType, callback) {
-        callAPI('/location_details', {
-            entity_id: entityId,
-            entity_type: entityType
-        }, callback);
-    };
-
-    this.getLocations = function(placeName, callback) {
-        callAPI('/locations', {
-            query: placeName,
-        }, callback);
-    };
-
-
-    // Restraunt Specific APIs
-    this.getDailyMenu = function(restaurantId, callback) {
-        callAPI('/dailymenu', {
-            res_id: restaurantId
-        }, callback);
-    };
-
-    this.getRestaurant = function(restaurantId, callback) {
-        console.log(restaurantId);
-        callAPI('/restaurant', {
-            res_id: restaurantId
-        }, callback);
-    };
-
-    this.getReviewsForRestaurant = function(restaurantId, options, callback) {
-        var qs = options;
-        qs.res_id = restaurantId;
-        callAPI('/reviews', qs, callback);
-    };
-
-    this.search = function(options, callback) {
-        console.log(options.entity_id);
-        console.log(options.entity_type);
-        callAPI('/search', options, callback);
     };
 
 };
