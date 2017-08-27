778488ad64fea80252d07024249c31d0c74e6a9c
Prakhar Rastogi <rastogi.prakhar@live.com>
2017-08-27 23:11:26 +0530

    redis host resolved

--- controller/youtubeController.js
+++ controller/youtubeController.js
@@ -4,8 +4,9 @@ var YOUTUBE_ACCESS_TOKEN = process.env.YOUTUBE_ACCESS_TOKEN;
 
 const redis = require('redis');
 const REDIS_PORT = process.env.REDIS_PORT || 18315;
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
 
-const client = redis.createClient(REDIS_PORT);
+const client = redis.createClient(REDIS_PORT, REDIS_HOST);
 
 client.on("error", function (err) {
     console.log("Error " + err);
